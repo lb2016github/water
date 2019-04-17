5aa9770a45673f939f4feefbed890070ece83736
LuoBo <HelloLuoBo@126.com>
2019-04-17 20:02:49 +0800

    modify log module

--- src/common/log.h
+++ src/common/log.h
@@ -1,35 +1,14 @@
 #ifndef WATER_LOG_H
 #define WATER_LOG_H
 #include <iostream>
+#include <stdarg.h>
 
 #define MAX_LOG_LEN 128
 
 namespace water {
-
-#define LOG(msg, format) \
-	int start_idx = std::strlen(msg);\
-	va_list va;\
-	va_start(va, format);\
-	std::vsnprintf(msg + start_idx, MAX_LOG_LEN - start_idx, format, va);\
-	va_end(va);\
-	msg[MAX_LOG_LEN - 1] = '\0';\
-	std::cout << msg << std::endl;	// todo 重定向输出
-
-
-	void log_info(const char* format, ...) {
-		char msg[MAX_LOG_LEN] = "[Info]: ";
-		LOG(msg, format)
-	}
-
-	void log_warn(const char* format, ...) {
-		char msg[MAX_LOG_LEN] = "[Warn]: ";
-		LOG(msg, format)
-	}
-
-	void log_error(const char* format, ...) {
-		char msg[MAX_LOG_LEN] = "[Error]: ";
-		LOG(msg, format)
-	}
+	void log_info(const char* format, ...);
+	void log_warn(const char* format, ...);
+	void log_error(const char* format, ...);
 }
 
 #endif
