793918eb8dcb43c87516b5931e7ffef70e8acef2
LuoBo <HelloLuoBo@126.com>
2019-08-02 16:32:49 +0800

    for sync

--- src/render/mesh.cpp
+++ src/render/mesh.cpp
@@ -4,11 +4,25 @@ namespace water
 {
 	namespace render
 	{
-		void MeshDataCreator::create_box()
+		void MeshDataManager::create_cube()
 		{
+			struct Vertex {
+				Vertex(float p_x, float p_y, float p_z, float c_x, float c_y, float c_z) : position(p_x, p_y, p_z), color(c_x, c_y, c_z) {}
+				math3d::Vector3 position;
+				math3d::Vector3 color;
+			};
+			std::vector<Vertex> vertexs = {
+				{-1, 0, 0, 255, 0, 0},
+				{1, 0, 0, 255, 255, 0},
+				{0, 0, 2, 255, 0, 255},
+				{0, 2, 1, 0, 255, 0},
+			};
+
 
 		}
-
+		void MeshDataManager::add_data_to_buffer(std::vector<math3d::Vector3>& data, int & start_pos)
+		{
+		}
 	}
 }
 
--- src/render/mesh.h
+++ src/render/mesh.h
@@ -23,14 +23,13 @@ namespace water
 			MeshData() : base_pos(-1), base_normal(-1), base_coord(-1), base_tangent(-1), base_index(-1), num_vertex(-1), num_index(-1) {}
 		};
 
-		struct MeshDataCreator
-		{
-			void create_box();
-		};
-
 
 		class MeshDataManager
 		{
+		public:
+			void create_cube();
+			void add_data_to_buffer(std::vector<math3d::Vector3>& data, int& start_pos);
+
 		private:
 			std::map<std::string, MeshData> m_data;
 			std::vector<math3d::Vector3> m_vec3_buffer;
