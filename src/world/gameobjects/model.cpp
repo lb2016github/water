140677053b38a8d702a04da0f82bd839cd7676f8
LuoBo <HelloLuoBo@126.com>
2019-08-28 17:40:22 +0800

    implement load model

--- src/world/gameobjects/model.cpp
+++ src/world/gameobjects/model.cpp
@@ -2,6 +2,8 @@
 #include "world/components/component_const.h"
 #include "world/components/mesh_component.h"
 #include "world/components/material_component.h"
+#include "filesystem/xml_file.h"
+#include "common/log.h"
 
 namespace water
 {
@@ -41,14 +43,36 @@ namespace water
 		render::DrawCommand Model::get_draw_command()
 		{
 			render::DrawCommand dc;
-			dc.draw_mode = render::TRIANGLE_TRIP;
+			dc.draw_mode = render::TRIANGLES;
 			return dc;
 		}
 		void Model::load_from_file(std::string & filename)
 		{
-			// load mesh
-			MeshComponent* mesh_comp = (MeshComponent*)get_component(COMP_MESH);
-			mesh_comp->load_from_file(filename);
+			filesystem::XMLFile xml_file;
+			xml_file.load(filename.c_str());
+			if (xml_file.m_loaded != true)
+			{
+				log_error("[MODEL]Fail to load file %s\n", filename.c_str());
+				return;
+			}
+			auto root_node = xml_file.get_root_node();
+			for each (auto child in root_node.children())
+			{
+				// load mesh
+				if (child.name == "Mesh")
+				{
+					std::string filepath = child.attribute("path").as_string();
+					MeshComponent* mesh_comp = (MeshComponent*)get_component(COMP_MESH);
+					mesh_comp->load_from_file(filename);
+				}
+				// load material
+				else if (child.name == "Material")
+				{
+					std::string filepath = child.attribute("path").as_string();
+					MaterialComponent* comp = (MaterialComponent*)get_component(COMP_MATERIAL);
+					comp->load_from_file(filepath);
+				}
+			}
 		}
 	}
 }
