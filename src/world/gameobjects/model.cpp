f78508691207662a53575ba0bbfef6b37319b2dc
LuoBo <HelloLuoBo@126.com>
2019-09-18 22:08:36 +0800

    speed up loading mesh

--- src/world/gameobjects/model.cpp
+++ src/world/gameobjects/model.cpp
@@ -22,16 +22,21 @@ namespace water
 			}
 			auto root_node = xml_file.get_root_node();
 			root_node = root_node.first_child();
-			ModelPtr model = std::dynamic_pointer_cast<Model>(shared_from_this());
+
+			std::vector<render::MeshDataPtr> cached_mesh;
+			std::string cached_mesh_path = "";
 			for each (auto sub_model in root_node.children()) // is SubModel Node
 			{
-				if (model.get() != this)
+				int index = sub_model.attribute("index").as_int();
+				ModelPtr model;
+				if (index == 0)
 				{
+					model = std::dynamic_pointer_cast<Model>(shared_from_this());
+				}
+				else
+				{
+					model = std::make_shared<Model>();
 					this->add_child(model);
-					auto trans_comp = (TransformComponent*)model->get_component(COMP_TRANSFORMATION);
-					trans_comp->position = math3d::Vector3(0, 0, 0);
-					trans_comp->scale = math3d::Vector3(1, 1, 1);
-					trans_comp->rotation = math3d::Vector3(0, 0, 0);
 				}
 				for each(auto child in sub_model.children())
 				{
@@ -41,9 +46,12 @@ namespace water
 						std::string filepath = child.attribute("path").as_string();
 						int mesh_idx = child.attribute("mesh_idx").as_int();
 						auto mesh_comp = (MeshComponent*)model->get_component(COMP_MESH);
-						auto meshes = mesh_comp->load_from_file(filepath, false);
-						assert(meshes.size() > mesh_idx);
-						mesh_comp->set_mesh_ptr(meshes[mesh_idx]);
+						if (filepath != cached_mesh_path)
+						{
+							cached_mesh = mesh_comp->load_from_file(filepath, false);
+							cached_mesh_path = filepath;
+						}
+						mesh_comp->set_mesh_ptr(cached_mesh[mesh_idx]);
 					}
 					// load material
 					else if (strcmp(child.name(), "Material") == 0)
@@ -55,7 +63,6 @@ namespace water
 						comp->set_material(mat_map[mat_idx]);
 					}
 				}
-				model = std::make_shared<Model>();
 			}
 		}
 	}
