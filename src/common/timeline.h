0913ca35337670fef39676021abe364a72113716
LuoBo <HelloLuoBo@126.com>
2019-12-23 00:25:03 +0800

    modify timeline class

--- src/common/timeline.h
+++ src/common/timeline.h
@@ -1,64 +1,105 @@
 #ifndef WATER_TIMELINE_H
-#include <list>
-#include "adt.hpp"
 #include <set>
-#include "common.h"
-
 
 namespace water
 {
-	typedef void(*TimelineCallback)();
-	typedef unsigned int CallbackHandler;
-	struct TimelineCallbackWrapper
+	struct TimelineObserver
 	{
-		float m_time{ -1 };
-		TimelineCallback m_callback{ nullptr };
-		CallbackHandler m_handler{ -1 };
-
-		TimelineCallbackWrapper(float time, TimelineCallback cb);
-		TimelineCallbackWrapper(CallbackHandler handler);
-		TimelineCallbackWrapper(const TimelineCallbackWrapper& tcw);
-		bool operator<(const TimelineCallbackWrapper& tcw);
-		bool operator==(const TimelineCallbackWrapper& tcw);
-	private:
-		static CallbackHandler get_handler();
+		virtual void onTime(float curTime) = 0;
 	};
 
-	WaterInterface TimelineObserver
-	{
-		virtual void on_time(float time) = 0;
-		virtual void on_start() = 0;
-		virtual void on_end() = 0;
-		virtual void on_cancel() = 0;
-	};
-
+	/* 
+	@Desc Timeline
+	@Property m_duration, if m_duration < 0, then m_duration is infinate
+	@Property m_curTime
+	@Property m_speedRate, speed rate of timeline, 
+		if m_speedRate < 0, then timeline goes in reverse direction; 
+		else if 0 < m_spped_rate < 1£¬timeline moves slow; 
+		else m_speedRate > 1£¬timeline moves fast
+	@Property m_loop
+	@Property m_playing
+	the logic of ending timeline when it is not looped
+		if m_speedRate < 0:
+			return m_curTime <= 0;
+		else:
+			return m_curTime >= m_duration;
+	the logic of updating current time:
+		m_curTime += deltaTime * m_speedRate;
+	*/
 	class Timeline
 	{
 	protected:
-		// duration and current time
-		float m_duration{ 0 };
-		float m_time{ 0 };
-		float m_ratio{ 1.0 };	// ratio of timeline
-		bool m_loop{ false };
-		// tag of status
-		bool m_paused{ false };
-		bool m_started{ false };
-		bool m_ended{ false };
+		// duration of timeline
+		float m_duration{ -1 };
+		// current time of timeline
+		float m_curTime{ 0 };
+		// speed rate of timeline
+		float m_speedRate{ 1.0 };
+		// whether timeline is loop
+		float m_loop{ false };
+		// whether timeline is playing
+		float m_playing{ false };
 		// observers
-		std::set<TimelineObserver*> m_observer_set;
+		std::set<TimelineObserver*> m_sObserver;
+	public:
+		Timeline(float duration);
+		Timeline(float duration, bool loop);
+		Timeline(float duration, bool loop, float speedRate);
+		~Timeline();
+	protected:
+		void virtual onTick();
 
 	public:
-		void start();
-		void stop();
+		/*
+		play timeline from 0
+		*/
+		void play();
+		/*
+		replay timeline
+		*/
+		void replay();
+		/*
+		pause timeline
+		*/
 		void pause();
-		void resume();
-		
-		// users should call tick
-		void tick(float delta_time);
-		void set_ratio(float ratio);
-		void add_observer(TimelineObserver* observer);
-		void remove_observer(TimelineObserver* observer);
+		/*
+		tick
+		*/
+		void tick(float deltaTime);
+		/*
+		get current time
+		*/
+		float getCurrentTime();
+		/*
+		set current time
+		*/
+		void setCurrentTime(float time);
+		/*
+		set current time by ratio, the ratio is in range[0, 1];
+		*/
+		void setCurrentTimeRatio(float timeRatio);
+		/*
+		set loop
+		*/
+		void setLoop(bool loop);
+		/*
+		add observer
+		*/
+		void addObserver(TimelineObserver* obs);
+		/*
+		remove observer
+		*/
+		void removeObserver(TimelineObserver* obs);
+		/*
+		set speed rate
+		*/
+		void setSpeedRate(float speedRate);
+		/*
+		get speed rate
+		*/
+		float getSpeedRate();
+	private:
+		void _informTime(float time);
 	};
 }
-
 #endif // !WATER_TIMELINE_H
--- src/world/animation/animator.h
+++ src/world/animation/animator.h
@@ -10,31 +10,13 @@ namespace water
 {
 	namespace world
 	{
-		class Animator
+		class TimelineAnimator: public Timeline
 		{
 		private:
 			// animation data
 			SkeletonAnimationClipDataPtr m_pSkelAnimClipData;
 			// cur anim name
 			std::string m_sCurAnim;
-		public:
-			void tick(float delta);
-		};
-		// helper of playing an animation
-		class AnimationPlayHelper: TimelineObserver
-		{
-		public:
-			AnimationPlayHelper();
-			~AnimationPlayHelper();
-		protected:
-			virtual void on_time(float time);
-			virtual void on_start();
-			virtual void on_end();
-			virtual void on_cancel();
-		private:
-			float pre_time = { -1 };
-			std::vector<TimelineCallbackWrapper> m_callbacks;
-			Timeline m_timeline;
 		};
 	}
 }
