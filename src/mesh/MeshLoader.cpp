b193afad3d9d95ba133849746e2dc3dfcc5d4693
LuoBo <HelloLuoBo@126.com>
2019-12-26 19:41:14 +0800

    tmp code for loading animation

--- src/mesh/MeshLoader.cpp
+++ src/mesh/MeshLoader.cpp
@@ -191,7 +191,7 @@ namespace water
 			}
 			return data_ptr;
 		}
-		const aiNode* getNodeByName(const aiNode* node, const char* name)
+		aiNode* getNodeByName(aiNode* node, const char* name)
 		{
 			if (strcmp(node->mName.C_Str(), name) ==0) return node;
 			for (int i = 0; i < node->mNumChildren; ++i)
@@ -218,6 +218,48 @@ namespace water
 			}
 		}
 
+		aiNode* getRootBone(aiNode* rootNode, const char* name)
+		{
+			aiNode* node = getNodeByName(rootNode, name);
+			while (node)
+			{
+				aiNode* parent = node->mParent;
+				if (!parent) return node;
+				std::string pName = parent->mName.C_Str();
+				if (pName.find(name) == 0 && pName.find("Assimp") != std::string::npos)
+				{
+					return node;
+				}
+				else
+				{
+					node = parent;
+				}
+			}
+			return nullptr;
+		}
+
+		world::SkeletonPtr createSkeletonByRootBone(aiNode* rootNode)
+		{
+			// hierachy first 
+			std::vector<world::Joint> joints;
+			auto node = rootNode;
+			int parentIndex = -1;
+			do
+			{
+				world::Joint joint;
+				joint.m_name = node->mName.C_Str();
+				auto& mtx = node->mTransformation;
+				joint.m_parentIndex = parentIndex;
+				joint.m_invBindPose = math3d::Matrix(
+					mtx.a1, mtx.a2, mtx.a3, mtx.a4,
+					mtx.b1, mtx.b2, mtx.b3, mtx.b4,
+					mtx.c1, mtx.c2, mtx.c3, mtx.c4,
+					mtx.d1, mtx.d2, mtx.d3, mtx.d4
+					);
+				joints.push_back(std::move(joint));
+			} while (node);
+		}
+
 		world::AnimationClipData MeshLoader::load_animation(const std::string& filename)
 		{
 			auto abs_path = filesystem::FileSystem::get_instance()->get_absolute_path(filename);
@@ -232,9 +274,15 @@ namespace water
 				auto meshPtr = scene->mMeshes[i];
 				std::vector<world::Joint*> joints;
 				if (meshPtr->mNumBones <= 0) continue;
+				world::SkeletonPtr skePtr = nullptr;
 				for (int j = 0; j < meshPtr->mNumBones; ++j)
 				{
 					auto bone = meshPtr->mBones[j];
+					// init skeleton
+					if (!skePtr)
+					{
+						aiNode* rootBoneNode = getRootBone(scene->mRootNode, bone->mName.C_Str());
+					}
 					world::Joint* joint = new world::Joint();
 					joint->m_name = bone->mName.C_Str();
 					auto& mtx = bone->mOffsetMatrix;
@@ -246,7 +294,6 @@ namespace water
 						);
 					joints.push_back(joint);
 				}
-
 			}
 
 
